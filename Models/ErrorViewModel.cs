0d51c2d9a16a2fb4eb49da15384110b0f93f3eea
pier <pierknows@gmail.com>
2023-07-28 13:46:28 -0400

    initial project set up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HairSalon.Solution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
