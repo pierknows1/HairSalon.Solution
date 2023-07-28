27ec3611a1c70f7fca0b3e18db3c5d94c1df3477
pier <pierknows@gmail.com>
2023-07-28 15:44:54 -0400

    program.cs has some rooting issues

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-
-namespace HairSalon.Solution.Controllers;
-
-public class HomeController : Controller
+namespace HairSalon.Controllers
 {
- 
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+        return View();
+        }
+    }
 }
---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,1 @@
+namespace HairSalon.Models
