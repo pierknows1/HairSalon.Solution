6671cbcc14e562fcdb9a9b43ad702e249ebc1d77
pier <pierknows@gmail.com>
2023-07-28 20:30:36 -0400

    landing page loads

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
     {
         public int ClientId { get; set; }
         public string Name { get; set; }
-        public int CustomerId { get; set; }
+        public int StylistId { get; set; }
         public Stylist Stylist { get; set; }
     }
 }
--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -2,7 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models
 {
-    public class HairSalonContext : DbContext{
+    public class HairSalonContext : DbContext
+    {
         public DbSet<Stylist> Stylists { get; set; }
         public DbSet<Client> Clients { get; set; }
 
